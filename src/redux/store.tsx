1213dfe1e39a426a606edad008eb5db4a79fdb34
Alex <a.plyushchay@gmail.com>
2020-04-30 22:58:06 -0400

    set up linter and ts configs

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,11 +1,11 @@
-import { applyMiddleware, compose, createStore } from "redux";
-import thunk from "redux-thunk";
-import reducers from "./rootReducer";
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './rootReducer';
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV === `development`) {
-  const { logger } = require(`redux-logger`);
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger');
 
   middlewares.push(logger);
 }
