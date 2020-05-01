bd9cbf7661e5f1815475d3e10053e322b7ee1798
Alex <a.plyushchay@gmail.com>
2020-04-30 22:33:21 -0400

    Initial commit

---
+++ src/redux/store.tsx
@@ -0,0 +1,15 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./rootReducer";
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+}
+
+const enhancer = compose(applyMiddleware(...middlewares));
+
+export default createStore(reducers, enhancer);
