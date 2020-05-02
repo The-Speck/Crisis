e14c887e2ce8faa9d2598e1aa86322876359cc89
Alex <a.plyushchay@gmail.com>
2020-05-02 12:08:49 -0400

    change lint and format config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,32 +3,17 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
-    'prettier/@typescript-eslint',
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'airbnb',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint',
   ],
   rules: {
-    'react/jsx-filename-extension': [
-      2,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
-    // 'react/jsx-closing-bracket-location': [1, { selfClosing: 'after-props' }],
     '@typescript-eslint/no-var-requires': 0,
     'linebreak-style': 0,
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
+    'global-require': 0,
+    'arrow-body-style': ['error', 'as-needed'],
   },
   parserOptions: {
     project: './tsconfig.json',
