09eea19507c1ae72e6eaee745622636473a5130e
Alex <a.plyushchay@gmail.com>
2020-05-02 12:11:33 -0400

    divide config by environment

---
+++ src/config/index.tsx
@@ -0,0 +1,27 @@
+import ConfigException from '../errors/ConfigException';
+
+/**
+ * This file is structured in a specific way to satisify typescipt while maintaining
+ * dynamic loading for different environments. As more config options are added,
+ * we must expand the interface and object deconstructor below.
+ */
+
+export interface ConfigOptions {
+  NEWS_API_URL: string;
+  SERVER_URL: string;
+}
+
+let configOptions: ConfigOptions;
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    configOptions = require('./development');
+    break;
+  case 'production':
+    configOptions = require('./production');
+    break;
+  default:
+    throw new ConfigException(`Invalid Url Environment ${process.env.NODE_ENV}`);
+}
+
+export const { NEWS_API_URL, SERVER_URL } = configOptions;
---
+++ src/config/production/urls.tsx
@@ -0,0 +1,2 @@
+export const NEWS_API_URL = 'https://newsapi.org/v2/top-headlines';
+export const SERVER_URL = '';
