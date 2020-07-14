4457c825d68e2e4cd0d3b869d829c46ce8fe89da
Alex <a.plyushchay@gmail.com>
2020-07-14 09:02:40 -0400

    begin auth action

--- src/actions/auth.tsx
+++ src/actions/auth.tsx
@@ -1,40 +1,42 @@
-import { Action } from 'redux';
-import { DispatchableAction } from '.';
-import { ResponseStatus } from '../utils';
+// TODO: Implement auth with AsyncStorage.
 
-export enum AuthActionType {
-  RETRIEVING_USER = 'RETRIEVING_USER',
-  RECEIVE_USER = 'RECEIVE_USER',
-  ERROR_USER = 'ERROR_USER',
-}
+// import { Action } from 'redux';
+// import { DispatchableAction } from '.';
+// import { ResponseStatus } from '../utils';
 
-export const getUser = (options = defaultOptions): DispatchableAction<AuthActions, true> => async (
-  dispatch,
-): Promise<AuthActions> => {
-  await dispatch({
-    type: AuthActionType.RETRIEVING_TOP_HEADLINES,
-  });
+// export enum AuthActionType {
+//   RETRIEVING_USER = 'RETRIEVING_USER',
+//   RECEIVE_USER = 'RECEIVE_USER',
+//   ERROR_USER = 'ERROR_USER',
+// }
 
-  const response = await AuthHeadlines(Auth_API_URL, options);
+// export const getUser = (
+//   options = defaultOptions,
+// ): DispatchableAction<AuthActions, true> => async (dispatch): Promise<AuthActions> => {
+//   await dispatch({
+//     type: AuthActionType.RETRIEVING_TOP_HEADLINES,
+//   });
 
-  if (response.status === ResponseStatus.OK) {
-    return dispatch({
-      type: AuthActionType.RECEIVE_TOP_HEADLINES,
-      headlines: response.data,
-    });
-  } else {
-    return dispatch({
-      type: AuthActionType.ERROR_TOP_HEADLINES,
-      error: response.error,
-    });
-  }
-};
+//   const response = await AuthHeadlines(Auth_API_URL, options);
 
-export type AuthAction = Action<typeof AuthActionType.RETRIEVING_TOP_HEADLINES>;
-export interface AuthAction extends Action<typeof AuthActionType.RECEIVE_TOP_HEADLINES> {
-  headlines: Article[];
-}
-export interface AuthAction extends Action<typeof AuthActionType.ERROR_TOP_HEADLINES> {
-  error: string;
-}
-export type AuthActions = AuthAction | AuthAction | AuthAction;
+//   if (response.status === ResponseStatus.OK) {
+//     return dispatch({
+//       type: AuthActionType.RECEIVE_TOP_HEADLINES,
+//       headlines: response.data,
+//     });
+//   } else {
+//     return dispatch({
+//       type: AuthActionType.ERROR_TOP_HEADLINES,
+//       error: response.error,
+//     });
+//   }
+// };
+
+// export type AuthAction = Action<typeof AuthActionType.RETRIEVING_TOP_HEADLINES>;
+// export interface AuthAction extends Action<typeof AuthActionType.RECEIVE_TOP_HEADLINES> {
+//   headlines: Article[];
+// }
+// export interface AuthAction extends Action<typeof AuthActionType.ERROR_TOP_HEADLINES> {
+//   error: string;
+// }
+// export type AuthActions = AuthAction | AuthAction | AuthAction;
--- src/scenes/auth/index.tsx
+++ src/scenes/auth/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 
 const Auth = (): JSX.Element => {
+  console.log('in Auth component');
+
   return (
     <View>
       <Text>Hello World!</Text>
